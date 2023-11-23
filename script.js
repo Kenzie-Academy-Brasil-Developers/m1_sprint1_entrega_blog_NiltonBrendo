4062d40d3f9fb08caa5d53288acb7d63114e940a
Nilton <niltonbrendo4@gmail.com>
2023-11-23 13:43:08 -0300

    implementação section consultorio

--- script.js
+++ script.js
@@ -37,3 +37,21 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 }
+
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel() {
+    idx++;
+
+    if(idx > img.length - 1) {
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 300}px)`;
+}
+
+setInterval(carrossel, 3000);
